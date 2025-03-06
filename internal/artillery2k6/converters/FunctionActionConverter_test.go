6dad20f0aeb08ae1c225b85f9eaecc0381646b0a
Christopher Orchard <4171371+cjsaurusrex@users.noreply.github.com>
2025-03-06 22:12:59 +0000

    Add tests for Log and Function converters

---
+++ internal/artillery2k6/converters/FunctionActionConverter_test.go
@@ -0,0 +1,22 @@
+package converters
+
+import (
+	"github.com/cjsaurusrex/arillery2k6/internal/artillery2k6/models"
+	"testing"
+)
+
+func TestFunctionActionConverter_Convert_StatementIsCorrectFormat(t *testing.T) {
+	fac := &FunctionActionConverter{&models.FunctionAction{Name: "myFunction"}}
+	statement, _ := fac.Convert()
+	if statement[0] != "myFunction()" {
+		t.Errorf("Expected 'myFunction()', got %s", statement[0])
+	}
+}
+
+func TestFunctionActionConverter_Convert_NoImportsRequired(t *testing.T) {
+	fac := &FunctionActionConverter{&models.FunctionAction{Name: "myFunction"}}
+	_, imports := fac.Convert()
+	if len(imports) != 0 {
+		t.Errorf("Expected no imports, got %d", len(imports))
+	}
+}
---
+++ internal/artillery2k6/converters/LogActionConverter_test.go
@@ -0,0 +1,22 @@
+package converters
+
+import (
+	"github.com/cjsaurusrex/arillery2k6/internal/artillery2k6/models"
+	"testing"
+)
+
+func TestLogActionConverter_Convert_StatementIsCorrectFormat(t *testing.T) {
+	lac := &LogActionConverter{&models.LogAction{Value: "my test log"}}
+	statement, _ := lac.Convert()
+	if statement[0] != "console.log(\"my test log\")" {
+		t.Errorf("Expected 'console.log(\"my test log\")', got %s", statement[0])
+	}
+}
+
+func TestLogActionConverter_Convert_NoImportsRequired(t *testing.T) {
+	lac := &LogActionConverter{&models.LogAction{Value: "my test log"}}
+	_, imports := lac.Convert()
+	if len(imports) != 0 {
+		t.Errorf("Expected no imports, got %d", len(imports))
+	}
+}
